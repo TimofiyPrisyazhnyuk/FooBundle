d2f933625cfd54fa97c6595b702c0239efde473f
timofiyprisyazhnyuk <timofiyprisyazhnyuk@gmail.com>
2022-03-27 15:48:32 +0300

    Initial commit - added FooBundle simple console command for commands chain demonstration

---
+++ Command/HelloCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Bundle\FooBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class HelloCommand
+ *
+ * @author Timofiy Prisyazhnyuk <timofiyprisyazhnyuk@gmail.com>
+ * @version 1.0
+ */
+class HelloCommand extends Command
+{
+    /**
+     * @var string
+     */
+    protected static $defaultName = 'foo:hello';
+
+    /**
+     * Execute command method
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln('Hello from Foo!');
+
+        return Command::SUCCESS;
+    }
+}
---
+++ FooBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Bundle\FooBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class FooBundle
+ *
+ * @author Timofiy Prisyazhnyuk <timofiyprisyazhnyuk@gmail.com>
+ * @version 1.0
+ */
+class FooBundle extends Bundle
+{
+}
