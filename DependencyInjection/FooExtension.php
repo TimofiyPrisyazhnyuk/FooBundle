b1245d3c6c8cd6d9fe98408f8eb22bd8d8476ed8
timofiyprisyazhnyuk <timofiyprisyazhnyuk@gmail.com>
2022-03-29 10:06:12 +0300

    Register foo command with bundle services

---
+++ DependencyInjection/FooExtension.php
@@ -0,0 +1,24 @@
+<?php
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+/**
+ * Class FooExtension
+ *
+ * @author Timofiy Prisyazhnyuk <timofiyprisyazhnyuk@gmail.com>
+ * @version 1.0
+ */
+class FooExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
